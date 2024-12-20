28d5d0d8e4315a1bd5b7b6f43e3cb7ac712efd70
JUDE ASHIN <judeashin258@gmail.com>
2024-12-20 22:37:36 +0530

    changes for production

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,20 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(), // Load .env file or environment variables
     SequelizeModule.forRoot({
-      dialect: 'mysql', // or 'postgres', 'sqlite', etc.
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'sample',
+      dialect: 'postgres',
+      host: process.env.DB_HOST, // Use environment variables
+      port: 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       autoLoadModels: true,
-      synchronize: true,
+      synchronize: process.env.DB_SYNCHRONIZE === 'true', // For production, set false
     }),
     UsersModule,
   ],
